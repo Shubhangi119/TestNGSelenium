1b41908e022c074f05d4ae93d29d24d4c12e104d
Shubhangi Srivastava <shubhangisrivastava@qainfotech.com>
2020-02-22 18:33:42 +0530

    new branch1 changes

--- src/test/java/com/qait/TestNGSelenium/TestClass1.java
+++ src/test/java/com/qait/TestNGSelenium/TestClass1.java
@@ -18,17 +18,7 @@ public class TestClass1 {
 			System.out.println("Simple test-method One. Thread id is: " + id);
 		}
 
-		@Test
-		public void testMethodsTwo() {
-			long id = Thread.currentThread().getId();
-			System.out.println("Simple test-method Two. Thread id is: " + id);
-		}
-
-		@AfterMethod
-		public void afterMethod() {
-			long id = Thread.currentThread().getId();
-			System.out.println("After test-method. Thread id is: " + id);
-		}
+		
 	}
 
 
